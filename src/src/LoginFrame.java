f78c1b0b9b3ae1e2c27d66b603c93038598939a6
Trobanko <98237133+TrobankoDev@users.noreply.github.com>
2025-03-04 22:31:10 +0100

    Set up inital Login frame

---
+++ src/src/LoginFrame.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginFrame extends JFrame {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginButton, signupButton;
+
+    public LoginFrame() {
+        setTitle("LU-Connect Login");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel(new GridLayout(3, 2));
+
+        panel.add(new JLabel("Username:"));
+        usernameField = new JTextField();
+        panel.add(usernameField);
+
+        panel.add(new JLabel("Password:"));
+        passwordField = new JPasswordField();
+        panel.add(passwordField);
+
+        loginButton = new JButton("Login");
+        panel.add(loginButton);
+
+        signupButton = new JButton("Signup");
+        panel.add(signupButton);
+
+        add(panel);
+
+    }
+}
